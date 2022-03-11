9724073467ea2a40626cee97ddf39c9c341d3991
Erick Branner <eobrann@gmail.com>
2022-03-11 14:40:39 -0800

    google calendar api request working

--- app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.honorscollegeapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.honorscollegeapp.R
+const val API_KEY = ""
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var requestQueue: RequestQueue
+    private val apiBaseUrl = "https://www.googleapis.com/"
+    private val calendarId = "dlme32b3csk7fjb1dpn9nhv5g8@group.calendar.google.com"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestQueue = Volley.newRequestQueue(this)
+        doCalendarSearch(this.calendarId)
+    }
+
+    private fun doCalendarSearch(calendarId: String){
+        val url =
+            "${apiBaseUrl}calendar/v3/calendars/${calendarId}/events?key=$API_KEY"
+
+        val req = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                Log.d("MainActivity", it)
+            },
+            {
+                Log.d(
+                    "MainActivity",
+                    "Error fetching from $url: ${it.message}"
+                )
+            }
+        )
+        requestQueue.add(req);
     }
 }
