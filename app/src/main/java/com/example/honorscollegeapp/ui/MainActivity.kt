d83fa91075ddc5fd9830ece33b22ec435ce21945
Erick Branner <eobrann@gmail.com>
2022-03-11 15:54:43 -0800

    api response data stored in data class

--- app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
@@ -5,29 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.honorscollegeapp.BuildConfig
 import com.example.honorscollegeapp.R
+import com.example.honorscollegeapp.data.Calendar
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
-/*
- * To use your own OpenWeather API key, create a file called `gradle.properties` in your
- * GRADLE_USER_HOME directory (this will usually be `$HOME/.gradle/` in MacOS/Linux and
- * `$USER_HOME/.gradle/` in Windows), and add the following line:
- *
- *   OPENWEATHER_API_KEY="<put_your_own_OpenWeather_API_key_here>"
- *
- * The Gradle build for this project is configured to automatically grab that value and store
- * it in the field `BuildConfig.OPENWEATHER_API_KEY` that's used below.  You can read more
- * about this setup on the following pages:
- *
- *   https://developer.android.com/studio/build/gradle-tips#share-custom-fields-and-resource-values-with-your-app-code
- *
- *   https://docs.gradle.org/current/userguide/build_environment.html#sec:gradle_configuration_properties
- *
- * Alternatively, you can just hard-code your API key below 🤷‍.
- */
 const val GOOGLE_API_KEY = "AIzaSyDjwke8tWWu_-CSsA7pev_G8OqTALsaAKs"
 
 class MainActivity : AppCompatActivity() {
@@ -46,11 +31,18 @@ class MainActivity : AppCompatActivity() {
         val url =
             "${apiBaseUrl}calendar/v3/calendars/${calendarId}/events?key=$GOOGLE_API_KEY"
 
+        val moshi = Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+        val jsonAdapter: JsonAdapter<Calendar> =
+            moshi.adapter(Calendar::class.java)
+
         val req = StringRequest(
             Request.Method.GET,
             url,
             {
                 Log.d("MainActivity", it)
+                val results = jsonAdapter.fromJson(it)
             },
             {
                 Log.d(
