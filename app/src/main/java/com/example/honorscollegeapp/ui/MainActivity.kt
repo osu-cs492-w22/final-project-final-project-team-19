3fd08a6818f3ab4b976972e32011e56c01d1b7cc
Erick Branner <eobrann@gmail.com>
2022-03-11 15:16:37 -0800

    hard coded api key

--- app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
@@ -8,8 +8,27 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.honorscollegeapp.BuildConfig
 import com.example.honorscollegeapp.R
-const val API_KEY = ""
+
+/*
+ * To use your own OpenWeather API key, create a file called `gradle.properties` in your
+ * GRADLE_USER_HOME directory (this will usually be `$HOME/.gradle/` in MacOS/Linux and
+ * `$USER_HOME/.gradle/` in Windows), and add the following line:
+ *
+ *   OPENWEATHER_API_KEY="<put_your_own_OpenWeather_API_key_here>"
+ *
+ * The Gradle build for this project is configured to automatically grab that value and store
+ * it in the field `BuildConfig.OPENWEATHER_API_KEY` that's used below.  You can read more
+ * about this setup on the following pages:
+ *
+ *   https://developer.android.com/studio/build/gradle-tips#share-custom-fields-and-resource-values-with-your-app-code
+ *
+ *   https://docs.gradle.org/current/userguide/build_environment.html#sec:gradle_configuration_properties
+ *
+ * Alternatively, you can just hard-code your API key below 🤷‍.
+ */
+const val GOOGLE_API_KEY = "AIzaSyDjwke8tWWu_-CSsA7pev_G8OqTALsaAKs"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var requestQueue: RequestQueue
@@ -25,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun doCalendarSearch(calendarId: String){
         val url =
-            "${apiBaseUrl}calendar/v3/calendars/${calendarId}/events?key=$API_KEY"
+            "${apiBaseUrl}calendar/v3/calendars/${calendarId}/events?key=$GOOGLE_API_KEY"
 
         val req = StringRequest(
             Request.Method.GET,
