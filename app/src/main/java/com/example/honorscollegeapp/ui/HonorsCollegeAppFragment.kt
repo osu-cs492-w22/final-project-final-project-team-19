a0cc6f63166372a10e9b687480532718a3eb9fd8
AJ-Protzel <ajprotzel@gmail.com>
2022-03-12 14:49:09 -0800

    added baseline xml
    added basic xml for display main activity and outlines what pages there will be in the nav graph. event cards should be hookable and display like the weather forecasts on this page

--- app/src/main/java/com/example/honorscollegeapp/ui/HonorsCollegeAppFragment.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/HonorsCollegeAppFragment.kt
@@ -3,29 +3,73 @@ package com.example.honorscollegeapp.ui
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.honorscollegeapp.R
 import com.example.honorscollegeapp.data.Calendar
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 const val GOOGLE_API_KEY = "AIzaSyDjwke8tWWu_-CSsA7pev_G8OqTALsaAKs"
 
-class HonorsCollegeAppFragment : Fragment(R.layout.honors_college_app) {
+/* Rest of onCreate body */
+//private val mainAdapter = MainActivityAdapter(::onClick)
+//private val viewModel: MainActivityViewModel by viewModels()
+private lateinit var searchBoxET: EditText
+private lateinit var searchResultsListRV: RecyclerView
+private lateinit var searchErrorTV: TextView
+private lateinit var loadingIndicator: CircularProgressIndicator
+/* Rest of onCreate body */
+
+class HonorsCollegeAppFragment : Fragment(R.layout.main_home) {
     private lateinit var requestQueue: RequestQueue
     private val apiBaseUrl = "https://www.googleapis.com/"
     private val calendarId = "dlme32b3csk7fjb1dpn9nhv5g8@group.calendar.google.com"
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?
-    ) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         /* Rest of onCreate body */
+        searchBoxET = view.findViewById(R.id.et_search_box)
+        searchResultsListRV = view.findViewById(R.id.rv_search_results)
+        searchErrorTV = view.findViewById(R.id.tv_search_error)
+        loadingIndicator = view.findViewById(R.id.loading_indicator)
+        searchResultsListRV.layoutManager = LinearLayoutManager(requireContext())
+        searchResultsListRV.setHasFixedSize(true)
+//        searchResultsListRV.adapter = mainAdapter
+//        viewModel.searchResults.observe(viewLifecycleOwner) { searchResults ->
+//            mainAdapter.updateRepoList(searchResults)
+//        }
+//        viewModel.loadingStatus.observe(viewLifecycleOwner) { uiState ->
+//            when (uiState) {
+//                LoadingStatus.LOADING -> {
+//                    loadingIndicator.visibility = View.VISIBLE
+//                    searchResultsListRV.visibility = View.INVISIBLE
+//                    searchErrorTV.visibility = View.INVISIBLE
+//                }
+//                LoadingStatus.ERROR -> {
+//                    loadingIndicator.visibility = View.INVISIBLE
+//                    searchResultsListRV.visibility = View.INVISIBLE
+//                    searchErrorTV.visibility = View.VISIBLE
+//                }
+//                else -> {
+//                    loadingIndicator.visibility = View.INVISIBLE
+//                    searchResultsListRV.visibility = View.VISIBLE
+//                    searchErrorTV.visibility = View.INVISIBLE
+//                }
+//            }
+//        }
+        /* Rest of onCreate body */
+
         requestQueue = Volley.newRequestQueue(requireContext())
         doCalendarSearch(this.calendarId)
     }
--- app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.honorscollegeapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.honorscollegeapp.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
