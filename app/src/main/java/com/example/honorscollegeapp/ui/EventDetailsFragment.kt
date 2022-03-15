2d128129fef963bd3b12f4ac662502e8725faecf
Erick Branner <eobrann@gmail.com>
2022-03-14 18:53:05 -0700

    event details page shows correct information

--- app/src/main/java/com/example/honorscollegeapp/ui/EventDetailsFragment.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/EventDetailsFragment.kt
@@ -1,8 +1,21 @@
 package com.example.honorscollegeapp.ui
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.honorscollegeapp.R
 
 class EventDetailsFragment : Fragment(R.layout.page_event_detail) {
+    private val args: EventDetailsFragmentArgs by navArgs()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.tv_event_title).text = args.event.summary
+        view.findViewById<TextView>(R.id.tv_event_description).text = args.event.description
+        view.findViewById<TextView>(R.id.tv_event_location).text = args.event.location
+        view.findViewById<TextView>(R.id.tv_event_start).text = args.event.start.dateTime
+        view.findViewById<TextView>(R.id.tv_event_end).text = args.event.end.dateTime
+    }
 }
