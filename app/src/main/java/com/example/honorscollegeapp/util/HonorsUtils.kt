1a61d99cff9f27cfad190c734bd2bd57ddcb50f9
Aj Protzel <31741271+AJ-Protzel@users.noreply.github.com>
2022-03-16 11:36:20 -0700

    added detail background merged datetime-utils

--- app/src/main/java/com/example/honorscollegeapp/util/HonorsUtils.kt
+++ app/src/main/java/com/example/honorscollegeapp/util/HonorsUtils.kt
@@ -1,13 +1,19 @@
 package com.example.honorscollegeapp.util
 
-// 2022-02-15T11:45:00-08:00
-
 fun HonorsUtilsDate(startDateTime: String): String {
     val dateArray = startDateTime.split("-", "T", ":").toTypedArray()
     return dateArray[1]+"/"+dateArray[2]
 }
 
 fun HonorsUtilsTime(startDateTime: String): String {
-    val dateArray = startDateTime.split("-", "T", ":").toTypedArray()
-    return dateArray[4] + ":" + dateArray[5]
+    var dateArray = startDateTime.split("-", "T", ":").toTypedArray()
+    var ampm = "AM"
+    if(dateArray[3].toInt() > 11){
+        dateArray[3] = (dateArray[3].toInt() - 12).toString()
+        ampm = "PM"
+    }
+    if(dateArray[3].toInt() == 0){
+        dateArray[3] = "12"
+    }
+    return dateArray[3] + ":" + dateArray[4] + ampm
 }
