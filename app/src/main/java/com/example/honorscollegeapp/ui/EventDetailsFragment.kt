2c419319b8d4c47d0481a4bc4abe962289b6520d
Aj Protzel <31741271+AJ-Protzel@users.noreply.github.com>
2022-03-16 17:44:34 -0700

    did the things

--- app/src/main/java/com/example/honorscollegeapp/ui/EventDetailsFragment.kt
+++ app/src/main/java/com/example/honorscollegeapp/ui/EventDetailsFragment.kt
@@ -6,16 +6,22 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.honorscollegeapp.R
+import com.example.honorscollegeapp.util.HonorsUtilsDate
+import com.example.honorscollegeapp.util.HonorsUtilsTime
 
 class EventDetailsFragment : Fragment(R.layout.page_event_detail) {
     private val args: EventDetailsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.tv_event_title).text = args.event.summary
-        view.findViewById<TextView>(R.id.tv_event_description).text = args.event.description
+
+        val date = HonorsUtilsDate(args.event.start.dateTime)
+        val time = HonorsUtilsTime(args.event.end.dateTime)
+
+//        view.findViewById<TextView>(R.id.tv_event_title).text = args.event.summary
+        view.findViewById<TextView>(R.id.tv_desc).text = args.event.description
         view.findViewById<TextView>(R.id.tv_event_location).text = args.event.location
-        view.findViewById<TextView>(R.id.tv_event_start).text = args.event.start.dateTime
-        view.findViewById<TextView>(R.id.tv_event_end).text = args.event.end.dateTime
+        view.findViewById<TextView>(R.id.tv_date).text = date
+        view.findViewById<TextView>(R.id.tv_time).text = time
     }
 }
